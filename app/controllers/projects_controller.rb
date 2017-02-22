d7abf722c324f88a247de88d0ba5fb5f7d4e9be4
Howard DeVennish <howarddevennish@gmail.com>
2017-02-22 01:15:20 +0000

    add form and view to create new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,31 @@
 class ProjectsController < ApplicationController
 
-  
+  get '/projects/new' do
+    erb :'/projects/create_project'
+  end
+
+  get '/projects' do
+    if logged_in?
+      load_projects
+      erb :'projects/projects'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/projects' do
+    project = Project.new
+    project.user_id = session[:user_id]
+    project.name = params[:name]
+    project.location = params[:location]
+    project.description = params[:description]
+    project.status = params[:status] if !params[:status].empty?
+    project.status = params[:notes] if !params[:notes].empty?
+    project.status = params[:tools] if !params[:tools].empty?
+    project.status = params[:materials] if !params[:materials].empty?
+    project.status = params[:priority] if !params[:priority].empty?
+    project.save
+    load_projects
+    redirect '/projects/projects'
+  end
 end
