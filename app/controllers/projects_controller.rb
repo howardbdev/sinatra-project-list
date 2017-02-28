45a2f57228607a56e4d311892982f960c8a47ab5
Howard DeVennish <howarddevennish@gmail.com>
2017-02-28 21:25:31 +0000

    build show project view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,19 +14,19 @@ class ProjectsController < ApplicationController
   end
 
   post '/projects' do
-    project = Project.new
-    project.user_id = session[:user_id]
-    project.name = params[:name]
-    project.location = params[:location]
-    project.description = params[:description]
-    project.status = params[:status] if !params[:status].empty?
-    project.status = params[:notes] if !params[:notes].empty?
-    project.status = params[:tools] if !params[:tools].empty?
-    project.status = params[:materials] if !params[:materials].empty?
-    project.status = params[:priority] if !params[:priority].empty?
-    if project.save
+    @project = Project.new
+    @project.user_id = session[:user_id]
+    @project.name = params[:name]
+    @project.location = params[:location]
+    @project.description = params[:description]
+    @project.status = params[:status]
+    @project.notes = params[:notes]
+    @project.tools = params[:tools]
+    @project.materials = params[:materials]
+    @project.priority = params[:priority]
+    if @project.save
       load_projects
-      redirect '/projects/projects'
+      erb :'/projects/show_project'
     else
       redirect '/projects/new'
     end
